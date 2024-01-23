16553bd5dc46a815b1e11636d65a7f807c756620
caforward <157344100+caforward@users.noreply.github.com>
2024-01-23 09:28:24 +0300

    Add files via upload

---
+++ pages/cession.php
@@ -0,0 +1,254 @@
+<?php
+    require_once("../php/headerinfocess.php");
+?>
+        <main id="main">
+            <!-- Contenedor -->
+			<div class="container-search">
+                <input id="dep" list="departe" class="pole_datalist  pole_datalist_first" placeholder="Поиск по номеру договора">
+                <datalist id="departe">
+                    <option value="тест1"></option>
+                </datalist>
+                <div class="container_element">
+                    <button id="btn" onerror="notFound()" onclick="zapros()"><i class="fas fa-search"></i></button>
+                </div>
+            </div>
+            <ul id="accordion-main" class="accordion-main">
+                <li class="open">
+                    <div class="link" id="#openli"><img src="../img/logobanks/sberbank_.png"><span> Наименование Банка </span><i class="fa fa-chevron-down chevron-down"></i></div>
+                    <ul class="submenu" style="overflow: hidden; display: block;">
+                        <div class="container-kontrakt" data-simplebar>
+                            <figure>
+                                <a href="https://caforward.ru/documents/svo.pdf" target="_blank">
+                                    <i class="far fa-file-pdf"></i>TEST1234
+                                </a>
+                            </figure>
+                            <figure>
+                                <a href="https://caforward.ru/documents/svo.pdf" target="_blank">
+                                    <i class="far fa-file-pdf"></i>TEST TEST TEST TEST
+                                </a>
+                            </figure>
+                            <figure>
+                                <a href="https://caforward.ru/documents/svo.pdf" target="_blank">
+                                    <i class="far fa-file-pdf"></i>TEST TEST TEST TEST
+                                </a>
+                            </figure>
+                            <figure>
+                                <a href="https://caforward.ru/documents/svo.pdf" target="_blank">
+                                    <i class="far fa-file-pdf"></i>TEST TEST TEST TEST
+                                </a>
+                            </figure>
+                            <figure>
+                                <a href="https://caforward.ru/documents/svo.pdf" target="_blank">
+                                    <i class="far fa-file-pdf"></i>TEST TEST TEST TEST
+                                </a>
+                            </figure>
+                            <figure>
+                                <a href="https://caforward.ru/documents/svo.pdf" target="_blank">
+                                    <i class="far fa-file-pdf"></i>TEST TEST TEST TEST
+                                </a>
+                            </figure>
+                            <figure>
+                                <a href="https://caforward.ru/documents/svo.pdf" target="_blank">
+                                    <i class="far fa-file-pdf"></i>TEST TEST TEST TEST
+                                </a>
+                            </figure>
+                            <figure>
+                                <a href="https://caforward.ru/documents/svo.pdf" target="_blank">
+                                    <i class="far fa-file-pdf"></i>TEST TEST TEST TEST
+                                </a>
+                            </figure>
+                            <figure>
+                                <a href="https://caforward.ru/documents/svo.pdf" target="_blank">
+                                    <i class="far fa-file-pdf"></i>TEST TEST TEST TEST
+                                </a>
+                            </figure>
+                            <figure>
+                                <a href="https://caforward.ru/documents/svo.pdf" target="_blank">
+                                    <i class="far fa-file-pdf"></i>TEST TEST TEST TEST
+                                </a>
+                            </figure>
+                            <figure>
+                                <a href="https://caforward.ru/documents/svo.pdf" target="_blank">
+                                    <i class="far fa-file-pdf"></i>TEST TEST TEST TEST
+                                </a>
+                            </figure>
+                            <figure>
+                                <a href="https://caforward.ru/documents/svo.pdf" target="_blank">
+                                    <i class="far fa-file-pdf"></i>TEST TEST TEST TEST
+                                </a>
+                            </figure>
+                            <figure>
+                                <a href="https://caforward.ru/documents/svo.pdf" target="_blank">
+                                    <i class="far fa-file-pdf"></i>TEST TEST TEST TEST
+                                </a>
+                            </figure>
+                            <figure>
+                                <a href="https://caforward.ru/documents/svo.pdf" target="_blank">
+                                    <i class="far fa-file-pdf"></i>TEST TEST TEST TEST
+                                </a>
+                            </figure>
+                            <figure>
+                                <a href="https://caforward.ru/documents/svo.pdf" target="_blank">
+                                    <i class="far fa-file-pdf"></i>TEST TEST TEST TEST
+                                </a>
+                            </figure>
+                            <figure>
+                                <a href="https://caforward.ru/documents/svo.pdf" target="_blank">
+                                    <i class="far fa-file-pdf"></i>TEST TEST TEST TEST
+                                </a>
+                            </figure>
+                            <figure>
+                                <a href="https://caforward.ru/documents/svo.pdf" target="_blank">
+                                    <i class="far fa-file-pdf"></i>TEST TEST TEST TEST
+                                </a>
+                            </figure>
+                            <figure>
+                                <a href="https://caforward.ru/documents/svo.pdf" target="_blank">
+                                    <i class="far fa-file-pdf"></i>TEST TEST TEST TEST
+                                </a>
+                            </figure>
+                            <figure>
+                                <a href="https://caforward.ru/documents/svo.pdf" target="_blank">
+                                    <i class="far fa-file-pdf"></i>TEST TEST TEST TEST
+                                </a>
+                            </figure>
+                            <figure>
+                                <a href="https://caforward.ru/documents/svo.pdf" target="_blank">
+                                    <i class="far fa-file-pdf"></i>TEST TEST TEST TEST
+                                </a>
+                            </figure>
+                            <figure>
+                                <a href="https://caforward.ru/documents/svo.pdf" target="_blank">
+                                    <i class="far fa-file-pdf"></i>TEST TEST TEST TEST
+                                </a>
+                            </figure>
+                            <figure>
+                                <a href="https://caforward.ru/documents/svo.pdf" target="_blank">
+                                    <i class="far fa-file-pdf"></i>TEST TEST TEST TEST
+                                </a>
+                            </figure>
+                        </div>
+                    </ul>
+                </li>
+                <li>
+                    <div class="link"><img src="../img/logobanks/logo_vneshekonombank.png"><span> Наименование Банка </span><i class="fa fa-chevron-down chevron-down"></i></div>
+                    <ul class="submenu">
+                        <div class="container-kontrakt" data-simplebar>
+                            <figure>
+                                <a href="https://caforward.ru/documents/svo.pdf" target="_blank">
+                                    <i class="far fa-file-pdf"></i>TEST TEST TEST TEST
+                                </a>
+                            </figure>
+                            <figure>
+                                <a href="https://caforward.ru/documents/svo.pdf" target="_blank">
+                                    <i class="far fa-file-pdf"></i>TEST TEST TEST TEST
+                                </a>
+                            </figure>
+                            <figure>
+                                <a href="https://caforward.ru/documents/svo.pdf" target="_blank">
+                                    <i class="far fa-file-pdf"></i>TEST TEST TEST TEST
+                                </a>
+                            </figure>
+                            <figure>
+                                <a href="https://caforward.ru/documents/svo.pdf" target="_blank">
+                                    <i class="far fa-file-pdf"></i>TEST TEST TEST TEST
+                                </a>
+                            </figure>
+                            <figure>
+                                <a href="https://caforward.ru/documents/svo.pdf" target="_blank">
+                                    <i class="far fa-file-pdf"></i>TEST TEST TEST TEST
+                                </a>
+                            </figure>
+                            <figure>
+                                <a href="https://caforward.ru/documents/svo.pdf" target="_blank">
+                                    <i class="far fa-file-pdf"></i>TEST TEST TEST TEST
+                                </a>
+                            </figure>
+                            <figure>
+                                <a href="https://caforward.ru/documents/svo.pdf" target="_blank">
+                                    <i class="far fa-file-pdf"></i>TEST TEST TEST TEST
+                                </a>
+                            </figure>
+                            <figure>
+                                <a href="https://caforward.ru/documents/svo.pdf" target="_blank">
+                                    <i class="far fa-file-pdf"></i>TEST TEST TEST TEST
+                                </a>
+                            </figure>
+                            <figure>
+                                <a href="https://caforward.ru/documents/svo.pdf" target="_blank">
+                                    <i class="far fa-file-pdf"></i>TEST TEST TEST TEST
+                                </a>
+                            </figure>
+                            <figure>
+                                <a href="https://caforward.ru/documents/svo.pdf" target="_blank">
+                                    <i class="far fa-file-pdf"></i>TEST TEST TEST TEST
+                                </a>
+                            </figure>
+                            <figure>
+                                <a href="https://caforward.ru/documents/svo.pdf" target="_blank">
+                                    <i class="far fa-file-pdf"></i>TEST TEST TEST TEST
+                                </a>
+                            </figure>
+                            <figure>
+                                <a href="https://caforward.ru/documents/svo.pdf" target="_blank">
+                                    <i class="far fa-file-pdf"></i>TEST TEST TEST TEST
+                                </a>
+                            </figure>
+                            <figure>
+                                <a href="https://caforward.ru/documents/svo.pdf" target="_blank">
+                                    <i class="far fa-file-pdf"></i>TEST TEST TEST TEST
+                                </a>
+                            </figure>
+                            <figure>
+                                <a href="https://caforward.ru/documents/svo.pdf" target="_blank">
+                                    <i class="far fa-file-pdf"></i>TEST TEST TEST TEST
+                                </a>
+                            </figure>
+                            <figure>
+                                <a href="https://caforward.ru/documents/svo.pdf" target="_blank">
+                                    <i class="far fa-file-pdf"></i>TEST TEST TEST TEST
+                                </a>
+                            </figure>
+                            <figure>
+                                <a href="https://caforward.ru/documents/svo.pdf" target="_blank">
+                                    <i class="far fa-file-pdf"></i>TEST TEST TEST TEST
+                                </a>
+                            </figure>
+                            <figure>
+                                <a href="https://caforward.ru/documents/svo.pdf" target="_blank">
+                                    <i class="far fa-file-pdf"></i>TEST TEST TEST TEST
+                                </a>
+                            </figure>
+                            <figure>
+                                <a href="https://caforward.ru/documents/svo.pdf" target="_blank">
+                                    <i class="far fa-file-pdf"></i>TEST TEST TEST TEST
+                                </a>
+                            </figure>
+                            <figure>
+                                <a href="https://caforward.ru/documents/svo.pdf" target="_blank">
+                                    <i class="far fa-file-pdf"></i>TEST TEST TEST TEST
+                                </a>
+                            </figure>
+                            <figure>
+                                <a href="https://caforward.ru/documents/svo.pdf" target="_blank">
+                                    <i class="far fa-file-pdf"></i>TEST TEST TEST TEST
+                                </a>
+                            </figure>
+                            <figure>
+                                <a href="https://caforward.ru/documents/svo.pdf" target="_blank">
+                                    <i class="far fa-file-pdf"></i>TEST TEST TEST TEST
+                                </a>
+                            </figure>
+                            <figure>
+                                <a href="https://caforward.ru/documents/svo.pdf" target="_blank">
+                                    <i class="far fa-file-pdf"></i>TEST TEST TEST TEST
+                                </a>
+                            </figure>
+                        </div>
+                    </ul>
+                </li>
+            </ul>
+        </main>
+<?php
+    require_once("../php/footerinfocess.php");
+?>
