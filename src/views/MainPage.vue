3d3ae8b041bbb9761a2f0884235ec193807fe67f
Vladislove <vlad.panov777@gmail.com>
2024-03-20 17:23:51 +0700

    init components HelpEver and AboutInNum

--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -1,9 +1,16 @@
 <template>
     <IntroSlider />
+    <AboutInNumbersVue />
+    <HelpEverClientVue />
 </template>
 
 <script setup>
+import AboutInNumbersVue from '../components/AboutInNumbers.vue';
+import HelpEverClientVue from '../components/HelpEverClient.vue';
 import IntroSlider from '../components/IntroSlider.vue';
+
 </script>
 
-<style></style>
+<style>
+
+</style>
