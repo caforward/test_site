a66d542edacf398f848302835ccde1c78fc38dbb
Shevja <vlsdki@gmail.ru>
2024-03-20 15:27:34 +0700

    inited offers component

---
+++ src/components/Offers.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h2>
+            Попрощайтесь с долгами онлайн на своих условиях
+        </h2>
+        <div>
+            <div>
+                <!-- form component -->
+            </div>
+            <div>
+                <!-- calc card component -->
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup></script>
+
+<style></style>
