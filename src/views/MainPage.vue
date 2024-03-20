81af2c4c04ddafebdeb5698d0e5fb0bbb2ca1a9b
Shevja <vlsdki@gmail.ru>
2024-03-20 15:17:22 +0700

    added header

--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -1,9 +1,13 @@
 <template>
-    <IntroSlider />
+    <Header />
+    <main>
+        <IntroSlider />
+    </main>
 </template>
 
 <script setup>
 import IntroSlider from '../components/IntroSlider.vue';
+import Header from '../layouts/Header.vue';
 </script>
 
 <style></style>
