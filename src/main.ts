71fd222a41fed77884933ae44b79074c725cf27f
Shevja <vlsdki@gmail.ru>
2024-04-09 00:25:57 +0700

    inited vue-router

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,19 @@
+import { createWebHistory, createRouter } from "vue-router";
 import { createApp } from 'vue'
 import './assets/scss/index.scss'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import MainPage from "./views/MainPage.vue";
+import About from "./views/About.vue";
+
+const routes = [
+    { name: 'home', path: "/", component: MainPage },
+    { name: 'about', path: "/about", component: About },
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+
+createApp(App).use(router).mount('#app')
---
+++ src/views/About.vue
@@ -0,0 +1,11 @@
+<template>
+    <Header />
+    <Footer />
+</template>
+
+<script setup>
+import Header from "../layouts/Header.vue";
+import Footer from '../layouts/Footer.vue';
+</script>
+
+<style></style>
