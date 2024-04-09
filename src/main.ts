a8f24dd954da54b52c656edd975d038eb16a0931
Vladislove <vlad.panov777@gmail.com>
2024-04-09 11:02:20 +0700

    vue-select library

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
+import 'vue-select/dist/vue-select.css';
 import './assets/scss/index.scss'
 import App from './App.vue'
+import  vSelect from 'vue-select'
 
-createApp(App).mount('#app')
+createApp(App)
+.component("v-select", vSelect)
+.mount('#app')
