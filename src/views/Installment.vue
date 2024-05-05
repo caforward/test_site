d416e3cf1f0f7b46eee62aef810b7c0657356a6d
Shevja <vlsdki@gmail.ru>
2024-05-06 02:54:14 +0700

    added debt form

--- src/views/Installment.vue
+++ src/views/Installment.vue
@@ -1,10 +1,12 @@
 <template>
     <main>
         <Intro />
+        <DebtForm />
     </main>
 </template>
 
 <script setup>
+import DebtForm from '../components/Installment/DebtForm.vue';
 import Intro from '../components/Installment/Intro.vue';
 </script>
 
