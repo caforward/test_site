0223ded5bce424d8d8fdad025e9f89f60144c3fe
Shevja <vlsdki@gmail.ru>
2024-05-07 04:26:57 +0700

    added content and adaptive

--- src/components/Installment/InstallmentCases.vue
+++ src/components/Installment/InstallmentCases.vue
@@ -1,8 +1,38 @@
 <template>
     <section class="section">
         <div class="container">
-            <div class="wrapper">
-                
+            <h2 class="title">
+                Рассрочка может стать доступной в следующих случаях:
+            </h2>
+            <div class="cards">
+                <div class="cards-card">
+                    <div class="cards-card__number">
+                        1
+                    </div>
+                    <div class="cards-card__content">
+                        <b>У вас нет достаточной суммы финансов</b><br>
+                        или собственности, чтобы погасить задолженность;
+                    </div>
+                </div>
+                <div class="cards-card">
+                    <div class="cards-card__number">
+                        2
+                    </div>
+                    <div class="cards-card__content">
+                        <b>Вы принимаете меры, чтобы оплатить долги:</b><br>
+                        это может быть продажа имущества, ведение бизнеса и т.д.
+                    </div>
+                </div>
+                <div class="cards-card">
+                    <div class="cards-card__number">
+                        3
+                    </div>
+                    <div class="cards-card__content">
+                        <b>Вы предоставляете в коллекторское агентство соглашение поручительства,</b>
+                        по которому другое лицо будет оплачивать ваш долг, если вы нарушите установленные соглашением
+                        обязательства.
+                    </div>
+                </div>
             </div>
         </div>
     </section>
@@ -12,18 +42,119 @@
 
 <style lang="scss" scoped>
 .section {
-    padding: 35px 0 80px;
+    padding: 80px 0;
 }
 
-.wrapper {
+.title {
+    margin-bottom: 35px;
+}
+
+.cards {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 30px;
+    padding-top: 30px;
+
+    &-card {
+        flex: 1;
+        width: 33%;
+        background-color: $gray-white;
+        border-radius: 30px;
+        position: relative;
+
+        &__number {
+            position: absolute;
+            top: 0;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            color: #fff;
+            border-radius: 100%;
+            width: 60px;
+            height: 60px;
+            background-color: $black-gray;
+            font-weight: 500;
+            font-size: 24px;
+        }
+
+        &__content {
+            padding: 50px 30px;
+            line-height: 156%;
+
+            b {
+                font-size: 18px;
+            }
+        }
+    }
 }
 
 @include desktop {
+    .title {
+        margin-bottom: 25px;
+    }
+
+    .cards {
+        &-card {
+            &__content {
+                line-height: 150%;
+                font-size: 14px;
+
+                b {
+                    font-size: 16px;
+                }
+            }
+        }
+    }
 }
 
 @include laptop {
+    .section {
+        padding: 60px 0;
+    }
+
+    .title {
+        margin-bottom: 10px;
+    }
+
+    .cards {
+        justify-content: center;
+
+        &-card {
+            width: 50%;
+            flex: 1;
+
+            &:nth-child(3) {
+                flex: none;
+            }
+
+            b {
+                font-size: unset;
+            }
+
+            &__content {
+                line-height: 171%;
+            }
+        }
+    }
 }
 
 @include mobile {
+    .section {
+        padding: 50px 0;
+    }
+
+    .cards {
+        gap: 50px;
+        flex-direction: column;
+
+        &-card {
+            width: 100%;
+            &:nth-child(3) {
+                width: 100%;
+            }
+        }
+    }
 }
 </style>
--- src/views/Installment.vue
+++ src/views/Installment.vue
@@ -1,15 +1,17 @@
 <template>
     <main>
         <Intro />
+        <InstallmentCases />
         <Agreement />
         <DebtForm />
     </main>
 </template>
 
 <script setup>
+import Intro from '../components/Installment/Intro.vue';
+import InstallmentCases from '../components/Installment/InstallmentCases.vue';
 import Agreement from '../components/Installment/Agreement.vue';
 import DebtForm from '../components/Installment/DebtForm.vue';
-import Intro from '../components/Installment/Intro.vue';
 </script>
 
 <style></style>
