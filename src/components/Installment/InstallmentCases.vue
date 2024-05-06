e55ae75e8e202f4520311342cb66753e6e046050
Shevja <vlsdki@gmail.ru>
2024-05-07 04:03:27 +0700

    init component

---
+++ src/components/Installment/InstallmentCases.vue
@@ -0,0 +1,29 @@
+<template>
+    <section class="section">
+        <div class="container">
+            <div class="wrapper">
+                
+            </div>
+        </div>
+    </section>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+.section {
+    padding: 35px 0 80px;
+}
+
+.wrapper {
+}
+
+@include desktop {
+}
+
+@include laptop {
+}
+
+@include mobile {
+}
+</style>
