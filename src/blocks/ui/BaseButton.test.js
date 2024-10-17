99d5544ede215c7c90265cfef1c49cfbe43312e1
Shevja <vlsdki@gmail.com>
2024-10-17 20:53:03 +0700

    removed inused files

--- src/blocks/ui/BaseButton.test.js
+++
@@ -1,9 +0,0 @@
-import { render } from '@testing-library/vue'
-
-import BaseButton from './BaseButton.vue'
-
-test('render test button', () => {
-    const { debug } = render(BaseButton)
-
-    debug()
-})
