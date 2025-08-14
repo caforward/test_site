b6567fd14c9337a04a3a9e2423c5d97a24b5d93d
Shevja <vlsdki@gmail.com>
2025-08-14 17:24:00 +0700

    Исправлена верстка письма, добавлен доступ к приватным данным через .env, перенесены php файлы в папку backend/

--- backend/public/email.php
+++ backend/public/email.php
@@ -6,6 +6,9 @@ use PHPMailer\PHPMailer\Exception;
 
 require 'vendor/autoload.php';
 
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $fields = [
         'name' => 'Имя',
@@ -21,25 +24,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         'paymentMonthlyDiscount' => 'Сумма ежемесячного платежа с учетом скидки в 5%',
         'fromComponent' => 'Компонент-инициатор письма',
     ];
-
-//     $htmlContent = '<html><head><meta charset="UTF-8"></head><body>';
-//     $htmlContent .= '<h2>Сообщение с сайта:</h2>';
-//
-//     foreach ($fields as $key => $label) {
-//         if (!empty($_POST[$key]) && $key !== 'fromComponent') {
-//             $htmlContent .= "<p><strong>$label:</strong> " . htmlspecialchars($_POST[$key], ENT_QUOTES, 'UTF-8') . "</p>";
-//         }
-//     }
-//
-//     $htmlContent .= '<small>Если письмо пустое, значит это ошибка, сообщите об этом руководителю отдела: скрипт письма - email.php.</small>';
-//     // Добавляем вывод компонента-инициатора письма
-//     if (!empty($_POST['fromComponent'])) {
-//         $htmlContent .= "<small><strong>Компонент-инициатор письма:</strong> " . htmlspecialchars($_POST['fromComponent'], ENT_QUOTES, 'UTF-8') . "</small>";
-//     } else {
-//         $htmlContent .= "<small><strong>Компонент-инициатор письма:</strong> Не указан</small>";
-//     }
-//
-//     $htmlContent .= '</body></html>';
 
     $rows = '';
     foreach($fields as $key => $label) {
@@ -87,12 +71,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     try {
         $mail->CharSet = 'UTF-8';
         $mail->isSMTP();
-        $mail->Host = 'smtp.mail.ru';
+        $mail->Host = $_ENV['SMTP_HOST'];
         $mail->SMTPAuth = true;
-        $mail->Username = 'site-no-reply@caforward.ru';
-        $mail->Password = '';
+        $mail->Username = $_ENV['SMTP_USER'];
+        $mail->Password = $_ENV['SMTP_PASS'];
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
-        $mail->Port = 465;
+        $mail->Port = $_ENV['SMTP_PORT'];
 
         $mail->setFrom('site-no-reply@caforward.ru', 'Сообщение с сайта');
         $mail->addAddress('dolg.info@caforward.ru');
---
+++ backend/public/rate.php
@@ -0,0 +1,63 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require 'vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $fields = [
+        'username' => 'Пользователь',
+        'rate' => 'Оценка',
+        'feedback' => 'Быстрый отзыв',
+        'message' => 'Предложения по улучшению от пользователя',
+    ];
+
+    $htmlContent = '<html><head><meta charset="UTF-8"></head><body>';
+    $htmlContent .= '<h2>Сообщение с сайта:</h2>';
+
+    foreach ($fields as $key => $label) {
+        if (!empty($_POST[$key])) {
+            $htmlContent .= "<p><strong>$label:</strong> " . htmlspecialchars($_POST[$key], ENT_QUOTES, 'UTF-8') . "</p>";
+        }
+    }
+    
+    $htmlContent .= '<small>Если письмо пустое, значит это ошибка, сообщите об этом руководителю отдела: скрипт письма - rate.php.</small>';
+    $htmlContent .= '</body></html>';
+
+    $mail = new PHPMailer(true);
+
+    try {
+        $mail->CharSet = 'UTF-8';
+        $mail->isSMTP();
+        $mail->Host = $_ENV['SMTP_HOST'];
+        $mail->SMTPAuth = true;
+        $mail->Username = $_ENV['SMTP_USER'];
+        $mail->Password = $_ENV['SMTP_PASS'];
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+        $mail->Port = $_ENV['SMTP_PORT'];
+
+        $mail->setFrom('site-no-reply@caforward.ru', 'Сообщение с сайта');
+        $mail->addAddress('dolg.info@caforward.ru');
+
+        $subject = 'Оценка сайта пользователем';
+        $mail->Subject = mb_encode_mimeheader($subject, 'UTF-8');
+        $mail->isHTML(true);
+        $mail->Body = $htmlContent;
+
+        $mail->send();
+        http_response_code(200);
+        echo json_encode(["message" => "Сообщение успешно отправлено!"]);
+    } catch (Exception $e) {
+        http_response_code(500);
+        echo json_encode(["message" => "Ошибка при отправке сообщения: " . $mail->ErrorInfo]);
+    }
+} else {
+    http_response_code(403);
+    echo json_encode(["message" => "Доступ запрещен"]);
+}
+?>
